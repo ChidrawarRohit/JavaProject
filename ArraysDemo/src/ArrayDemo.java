2f030be5dc07e4eef4cb60f7f5d07c725c355fd3
Hi <Hi@DESKTOP-1EOBG1J>
2022-11-24 16:18:40 +0530

    ArraysDemo

---
+++ ArraysDemo/src/ArrayDemo.java
@@ -0,0 +1,16 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) 
+	{
+		String names []= {"Rohit", "SriKanth", "Avinash"};
+		for (int i=0; i<names.length; i++) 
+		{
+			System.out.println("names[i]");
+		}
+	}
+}
+
+	
+
+
