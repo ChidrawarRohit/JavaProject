67e74d5ccbd6de07902a18d7ae2ea7bf36a8da60
Hi <Hi@DESKTOP-1EOBG1J>
2022-11-25 16:35:21 +0530

    DemoConcreteMethod2

---
+++ DemoConcreteMethod2/src/Maccess/DemoConcreteMethod2.java
@@ -0,0 +1,10 @@
+package Maccess;
+import Test.IClass;
+import Test.*;
+public interface DemoConcreteMethod2 {
+public static void main(String [] args) {
+	IClass ob = new IClass();
+	ob.m1(12);
+	ob.m2(13);
+}
+}
