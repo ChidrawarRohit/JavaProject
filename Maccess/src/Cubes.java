185fbb9a9e28b8dae57dcd508b868c527aa673e7
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 17:24:46 +0530

    Maccess

---
+++ Maccess/src/Cubes.java
@@ -0,0 +1,14 @@
+
+public class Cubes {
+
+	 public static void main (String ...arg)  {
+		for (int i=1;i<=10;i++) {
+
+			System.out.println(i*i*i);
+
+		}
+	}
+
+}
+
+
---
+++ Maccess/src/Decreases.java
@@ -0,0 +1,16 @@
+
+public class Decreases {
+
+	public static void main(String[] args)
+	{
+		int limit = 1;
+		int start = 20;
+
+		for (int n = start; n >= limit;n--) {
+			System.out.println(n);
+		}
+	}
+
+}
+
+
