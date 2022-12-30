6f71a6a09ee7f3f9f77001227356e0cbe8bf1454
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 16:54:58 +0530

    DemoPackage

---
+++ DemoPackage/src/CClass.java
@@ -0,0 +1,22 @@
+import maccess.PClass;
+public class CClass {
+
+	public static void main(String[] args) {
+		PClass p = new PClass();
+		p.sum();
+
+		PClass p1 = new PClass();
+		p1.sub();
+
+		PClass p2 = new PClass();
+		p1.mult();
+
+		PClass p3 = new PClass();
+		p1.div();
+
+		PClass p4 = new PClass();
+		p1.modulo();
+
+	}
+
+}
---
+++ DemoPackage/src/maccess/PClass.java
@@ -0,0 +1,26 @@
+package maccess;
+
+public class PClass {
+
+	int a,b,c;
+	public void Add()  {
+		a=50;
+		b=60;
+		c=10;
+	}
+public void sum()  {
+System.out.println("addition of two number = " +(50+60+10));
+	}
+public void sub()  {
+	System.out.println("sub of two number = " +(50-60-10));
+	}
+	public void mult ()  {
+	System.out.println("mult of two number = " +(50*60*10));
+	}
+	public void div()  {
+	System.out.println("div of two number = " +(50/60/10));
+	}
+	public void modulo()  {
+	System.out.println("modulo of two number = " +(50%60%10));
+	}
+}
