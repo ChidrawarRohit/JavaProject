78a3bc8587e46ffaabc2c863783222d38b79c2b9
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 17:29:58 +0530

    JavaProject

---
+++ JavaProject/src/Maccess/DemoStaticImport1.java
@@ -0,0 +1,14 @@
+package Maccess;
+import java.util.Scanner;
+import static java.lang.Math.*;
+public class DemoStaticImport1 {
+	public static void main(String [] args) {
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the value:");
+		double val = s.nextDouble();
+		double result = sqrt(val);
+		System.out.println("Sqrt of"+val+"is"+result);
+		s.close();
+	}
+}
+
