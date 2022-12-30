85b6bdfc5102d894d2a5a98277790a7a90a959fb
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 16:51:03 +0530

    DemoMethods1

---
+++ DemoMethods1/src/CClass/EvenNumbers.java
@@ -0,0 +1,11 @@
+package CClass;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int number=100;
+		System.out.println("Enter the even numbers 1 to "+"number+500");
+				for (int y=500; y<number; y++);
+	}
+
+}
