853d761f52b759295488c51190f9d835f42f4684
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 17:23:44 +0530

    MapDemo

---
+++ MapDemo/src/MapDemo.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class MapDemo {
+
+	public static void main(String[] args, String key, String value) {
+		HashMap<String , String> contacts = new HashMap<String , String>();
+		//contacts.put(key: "7207468501", value: "Rohit");
+	}
+
+}
