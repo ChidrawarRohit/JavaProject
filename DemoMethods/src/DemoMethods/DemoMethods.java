8cd9c9afcf888dd6cc92a17d811cf7eb1b97ea02
Hi <Hi@DESKTOP-1EOBG1J>
2022-12-30 16:46:47 +0530

    DemoMethods

---
+++ DemoMethods/src/DemoMethods/DemoMethods.java
@@ -0,0 +1,25 @@
+package DemoMethods;
+
+import java.util.Scanner;
+
+public class DemoMethods {
+
+	public static void main(String[] args)
+	{
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the marks of sub1:");
+		int s1 = s.nextInt();
+		System.out.println("Enter the marks of sub2:");
+		int s2 = s.nextInt();
+		System.out.println("Enter the marks of sub3:");
+		int s3 = s.nextInt();
+		System.out.println("Enter the marks of sub4:");
+		int s4 = s.nextInt();
+		System.out.println("Enter the marks of sub5:");
+		int s5 = s.nextInt();
+		System.out.println("Enter the marks of sub6:");
+		int s6 = s.nextInt();
+	}
+	
+
+}
